e504f340b64691a89590210e58e7ce218448da4a
mablancoalvarez <mablancoalvarez@gmail.com>
2020-04-27 17:58:08 +0200

    add button

--- src/components/Landing/AllPageHeader.js
+++ src/components/Landing/AllPageHeader.js
@@ -1,25 +1,41 @@
 import React from 'react';
 import logo from '../../images/awesome_logo.svg'
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 class AllPageHeader extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-    render() {
-          return (
-              <div id="allPageHeader" className="allPageHeader">
-                <header class="page_header">
-                  <Link to="/"><div className="header-image-container">
-                      <a href="./index.html" className="link-logo" title="Awesome profile-cards"/>
-                      <img className="header-awesome-image" src={logo} alt="Awesome profile-cards"
-                          title="Awesome profile-cards"/>
-                  </div>
-                  </Link>
-                </header>
-              </div>
-          );
-    }
+  constructor(props) {
+    super(props);
+    this.UpdateMode = this.UpdateMode.bind(this)
   }
+
+  UpdateMode(evt){
+    console.log(evt)
+    this.props.handleSwitchMode(evt.currentTarget.value)
+  }
+  render() {
+    return (
+      <div id="allPageHeader" className="allPageHeader">
+        <header class="page_header">
+          <Link to="/">
+            <div className="header-image-container">
+              <a href="./index.html" className="link-logo" title="Awesome profile-cards" />
+              <img className="header-awesome-image" src={logo} alt="Awesome profile-cards"
+                title="Awesome profile-cards" />
+
+            </div>
+          </Link>
+          <div className="button-container">
+            <label class="switch">
+              <input type="checkbox" onClick={this.UpdateMode} value={this.props.mode}/>
+              <span class="slider round"></span>
+            </label>
+          </div>
+
+
+        </header>
+      </div>
+    );
+  }
+}
 
 export default AllPageHeader;
--- src/components/ProfilePage/ComponentsPreview/ProfilePage.js
+++ src/components/ProfilePage/ComponentsPreview/ProfilePage.js
@@ -7,11 +7,31 @@ import AllPageFooter from '../../Landing/AllPageFooter';
 class ProfilePage extends React.Component {
     constructor(props) {
       super(props);
+      this.handleSwitchMode = this.handleSwitchMode.bind(this);
+      this.state = {
+        mode: false,
+      }
     }
+
+    
+    handleSwitchMode() {
+      this.setState(prevState => {
+        return {
+          mode: !prevState.mode
+        }
+      })
+    }
+
+
     render() {
+      console.log(this.state.mode)
           return (
               <div id="profilePage" className="profilePage">
-                  <AllPageHeader></AllPageHeader>
+                  <AllPageHeader
+                  handleSwitchMode= {this.handleSwitchMode}
+                  mode ={this.state.mode}
+                  />
+                  
                   <ProfilePageBody></ProfilePageBody>
                   <AllPageFooter></AllPageFooter>
               </div>
